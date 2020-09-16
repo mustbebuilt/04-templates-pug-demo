b4429da99cece2aca1fed5b1c9a9ae2e7dd81e48
mustbebuilt <martin@mustbebuilt.co.uk>
2020-09-16 19:21:03 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const routes = require('./routes/routes');
+
+// set up view engine
+
+app.use(express.static("./public"));
+
+app.use('/', routes())
+
+// remove for sample files
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!")
+})
+
+app.listen(3000);
+
+console.log("Express on 3000");
+
+module.exports = app;
